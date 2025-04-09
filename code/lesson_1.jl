1ffd5d37b652094538f4e25faa0bd43292691bbb
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2025-04-09 13:03:55 +0900

    Break lessons into smaller parts

--- code/lesson_1.jl
+++ code/lesson_1.jl
@@ -1,1 +1,13 @@
 # Blank for practice in the basic programming lesson
+using Test
+
+function f(x)
+    2 * x - 1
+end
+
+@test f(1) == 1
+@test f(2) == 3
+@test f(3) == 5
+@test f(4) == 7
+@test f(5) == 9
+@test f(19) == 37
