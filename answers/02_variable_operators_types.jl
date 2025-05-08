ec5491ab5c1abefc4a46c48018c032da331dc349
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2025-05-08 16:34:13 +0900

    Edit answers

--- answers/02_variable_operators_types.jl
+++ answers/02_variable_operators_types.jl
@@ -1,3 +1,13 @@
+# Exercise: seconds in 3 years
+s_per_min = 60
+m_per_hour = 60
+h_per_day = 24
+day_per_year = 365
+years = 3
+years * day_per_year * h_per_day * m_per_hour * s_per_min
+
+
+# Problem: check if x is between 10 and 20, inclusive
 x = 30
 if x > 10 || x < 20
     println("x is between 10 and 20")
