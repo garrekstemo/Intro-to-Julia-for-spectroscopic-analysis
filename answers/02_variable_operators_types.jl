67aa869a463eea1faf74c6f9301ea3707ffe9811
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2025-05-14 16:23:01 +0900

    Change problem 1 (required knowledge not yet introduced)

--- answers/02_variable_operators_types.jl
+++ answers/02_variable_operators_types.jl
@@ -7,6 +7,17 @@ years = 3
 years * day_per_year * h_per_day * m_per_hour * s_per_min
 
 
+# Problem: Write a short program that prints only the even numbers
+# between 1 and 10 that are not divisible by 4.
+
+# Learn about the % operator and use it in the following code:
+
+for i in 1:10
+    if i % 2 == 0 && i % 4 != 0  # students fill in this line
+        println(i)
+    end
+end
+
 # Problem: check if x is between 10 and 20, inclusive
 # Solution: use AND instead of OR. Change > and < to >= and <=, respectively.
 x = 10
