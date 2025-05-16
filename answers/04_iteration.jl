7a398aa9cfd9f080ecaa368cd526db1ce85e53e4
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2025-05-16 12:24:06 +0900

    Update iteration problems and solutions

---
+++ answers/04_iteration.jl
@@ -0,0 +1,19 @@
+# Problem 1
+# Two solutions (at least)
+
+# Solution 1
+total = 0
+n = 10
+for i in 1:n
+    total += i
+end
+total
+
+# Solution 2
+n = 10
+total = 0
+while n > 0
+    total += n
+    n -= 1
+end
+total
