e1054a572bc3f8203c262878b0ee79c971ea790a
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2025-05-16 12:21:42 +0900

    Update answers

--- answers/02_variable_operators_types.jl
+++ answers/02_variable_operators_types.jl
@@ -1,4 +1,4 @@
-# Exercise: seconds in 3 years
+# Problem 1: seconds in 3 years
 s_per_min = 60
 m_per_hour = 60
 h_per_day = 24
@@ -6,25 +6,8 @@ day_per_year = 365
 years = 3
 years * day_per_year * h_per_day * m_per_hour * s_per_min
 
+# Problem 2: Calculate pi
 
-# Problem: Write a short program that prints only the even numbers
-# between 1 and 10 that are not divisible by 4.
-
-# Learn about the % operator and use it in the following code:
-
-for i in 1:10
-    if i % 2 == 0 && i % 4 != 0  # students fill in this line
-        println(i)
-    end
-end
-
-# Problem: check if x is between 10 and 20, inclusive
-# Solution: use AND instead of OR. Change > and < to >= and <=, respectively.
-x = 10
-
-# if x > 10 || x < 20  -> incorrect code
-if x >= 10 && x <= 20  # corrected code
-    println("x is between 10 and 20")
-else
-    println("x is not between 10 and 20")
-end
+r = 5
+A = pi * r^2
+A = π * r^2
